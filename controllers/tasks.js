00ad8ae46fd07191bb62f2c19a3ec56400a9a769
sayihrudai <saihrudai11@gmail.com>
2022-05-28 10:54:24 +0530

    crud ✅

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,23 +1,65 @@
-const getAllTasks = (req, res) => {
-    res.send('All Tasks');
+const Task = require("../models/Task");
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({tasks});
+    } catch (error) {
+        res.status(500).json({msg:error});
+    }
 }
-const createTask = (req, res) => {
-    res.send('Create a Task');
-}
-const getTask = (req, res) => {
-    res.send('get a Task');
-}
-const deleteTask = (req, res) => {
-    res.send('delete a Task');
-}
-const updateTask = (req, res) => {
-    res.send('updated a Task');
-}
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+  res.send("delete task");
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {}
+};
 
 module.exports = {
-    getAllTasks,
-    createTask,
-    getTask,
-    deleteTask,
-    updateTask,
-}
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
