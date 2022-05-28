6e01bbcc83f708df50e1a2824dbd5e5999c9d827
sayihrudai <saihrudai11@gmail.com>
2022-05-28 10:27:27 +0530

    app.js added

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express(); 
+
+app.use(express.json());
+
+
+app.get("/hello", (req, res) => {
+    res.send('Task Manager App');
+})
+
+const port = 3000;
+
+
+app.listen(port, console.log(`server is listening on port ${port}...`));
